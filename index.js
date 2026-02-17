7bb41f0b0fc2ea70dd05031a4088e8d1c3859f1d
CherlyIC <cherlyimfurankunda@gmail.com>
2026-02-17 14:41:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// Exercise 1: Create an array of product categories
+const productCategories = ["Books", "Electronics", "Clothing", "Home & Kitchen", "Toys & Games"];
+
+console.log(productCategories[0]);
