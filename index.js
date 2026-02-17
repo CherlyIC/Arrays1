b09645bc64fbc142f31b84e1ca4c2a7b38b1bbcd
CherlyIC <cherlyimfurankunda@gmail.com>
2026-02-17 15:14:57 +0200

    Exercise 2

--- index.js
+++ index.js
@@ -13,4 +13,26 @@ console.log(productCategories.length);
 productCategories[1]= "pen" // pour la constante on peut la réassigner et non la déclarer à nouveau
 console.log(productCategories);
 
-//Exercise 2: 
+//Exercise 2: push,pop,shift,unshift,splice
+
+let inventory = [10,20,30];
+//a
+inventory.push(40);
+console.log(inventory);
+
+//b
+
+inventory.pop();
+console.log(inventory);
+//c
+
+inventory.unshift(0);
+console.log(inventory);
+//d
+inventory.shift();
+console.log(inventory);
+//e
+
+inventory.splice(1,1,15);
+console.log(inventory);
+
