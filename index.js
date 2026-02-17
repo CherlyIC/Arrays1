497e2ad3d65e53900203a27e18f83e09e18a3be6
CherlyIC <cherlyimfurankunda@gmail.com>
2026-02-17 15:03:05 +0200

    first Example

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Exercise 1: Create an array of product categories
 const productCategories = ["Books", "Electronics", "Clothing", "Home & Kitchen", "Toys & Games"];
-
+//a
 console.log(productCategories[0]);
+//b
+
+console.log(productCategories.length[productCategories.length - 1]);
+
+//c
+
+console.log(productCategories.length);
+//d
+productCategories[1]= "pen" // pour la constante on peut la réassigner et non la déclarer à nouveau
+console.log(productCategories);
+
+//Exercise 2: 
